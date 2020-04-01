201ca5fbf3c3bc9d1498aad2b9fcbaa6d41331ac
unknown <ghinea.alexandru.george@gmail.com>
2020-04-01 19:34:28 +0300

    last tests

--- tests.py
+++ tests.py
@@ -46,7 +46,7 @@ class MockObjectBasics(TestCase):
                     'calculate_minimum.return_value': 456,
                     'company.xyz.get_url.side_effect': Exception})
         self.assertEqual('owned', m.first_name)
-        self.assertEqual('notOwned', m.first_name)
+        #self.assertEqual('notOwned', m.first_name) #test pica deoarece atributul nu este corect
         self.assertEqual(456, m.calculate_minimum())
         self.assertRaises(Exception, m.company.xyz.get_url)
 
@@ -55,8 +55,44 @@ class MockObjectBasics(TestCase):
         m = Mock()
         self.assertFalse(m.called)
         m()
-        self.assertTrue(m.called)
+        #self.assertTrue(m.called)
+        print("Rezultatul  este %r" % self.assertTrue(m.called))
+        #print("Rezultatul  este %r" % self.assertFalse(m.called))
 
+    def test_mock_asserts(self):
+        """Obiectul mock are 4 helperi (assert_called_with, assert_called_once_with, assert_any_call, assert_has_calls),
+        se pot face diverse assert-uri si sa vedem cum a fost apelat un mock de cand a fost instantiat"""
+    
+        m = Mock()
+        m(1)
+
+        m.assert_called_with(1)
+        self.assertRaises(AssertionError, m.assert_called_with, 2)
+
+        # assert_called_with cel mai recent apel al obiectului Mock, nu conteaza daca a existat un apel vreodata
+        # apelul cu 1 va ridica eroare
+        m(2)
+        m.assert_called_with(2)
+        self.assertRaises(AssertionError, m.assert_called_with, 1)
+
+        # assert_called_once_with daca a fost apelat o singura data
+        # specified argument. ambele teste vor da fail
+        self.assertRaises(AssertionError, m.assert_called_once_with, 1)
+        self.assertRaises(AssertionError, m.assert_called_once_with, 2)
+
+        # assert_any_call daca a fost chemat cu argumentele respective vreodata de cand a fost creat obiectul
+        m.assert_any_call(1)
+        m.assert_any_call(2)
+        self.assertRaises(AssertionError, m.assert_called_with, 3)
+
+        # use assert_has_calls toate apeluri de cand a fost creat obiectul
+        m.assert_has_calls([mock.call(1), mock.call(2)])
+
+        #testul va da fail deoarece apelul a fost m(1), dupa m(2) si default any_order = False
+        self.assertRaises(AssertionError, m.assert_has_calls, [mock.call(2), mock.call(1)])
+
+        #  aici e adevarat deoarece nu conteaza ordinea in care a fost apelat obiectul Mock
+        m.assert_has_calls([mock.call(2), mock.call(1)], any_order=True)
     
 
 if __name__ == '__main__':
